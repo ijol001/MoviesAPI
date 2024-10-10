333fdbe7b19b9165d186b4feab27604fd1d4e39e
ijol001 <daisysarma20@gmail.com>
2024-10-10 17:51:53 +0530

    commit

--- index.mjs
+++ index.mjs
@@ -2,7 +2,7 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import movie from './model/movie.mjs';
 import cors from 'cors';
-
+import mongoose from 'mongoose';
 
 const app = express();
 
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 
 
@@ -48,9 +48,9 @@ app.get('/movie', async (req, res) => {
 //Read operation to fetch only one movie
 app.get('/movie/:id', async (req, res) => {
     const ID = req.params.id;
-        if(!mongoose.Types.ObjectId.isValid(ID)){
-           return res.status(400).json({error: "Invalid movie ID"}) 
-        }
+    if (!mongoose.Types.ObjectId.isValid(ID)) {
+        return res.status(400).json({ error: "Invalid movie ID" })
+    }
     try {
         const Movie = await movie.findById(ID);
         res.json(Movie);
@@ -58,29 +58,43 @@ app.get('/movie/:id', async (req, res) => {
     catch (error) {
         res.status(500).json({ message: error.message });
         console.log(error);
-                
+
     }
 });
 
 //update operation using movie id
-app.put('/movie/:id', async(req, res)=>{
-    try{
-     const updateMovie = await movie.findByIdAndUpdate(req.params.id, req.body, ({new: true}));
-     res.json(updateMovie);
+app.put('/movie/:id', async (req, res) => {
+    const ID = req.params.id;
+    if (!mongoose.Types.ObjectId.isValid(ID)) {
+        return res.status(400).json({ error: "Invalid movie ID" })
     }
-    catch(error){
-      res.status(500).json({ message: error.message});
+
+    if (!req.body || Object.keys(req.body).length === 0) {
+        return res.status(400).json({ error: "At least one field is required to update." });
+    }
+    try {
+        const updateMovie = await movie.findByIdAndUpdate(ID, req.body, ({ new: true }));
+        res.json(updateMovie);
+
+        if (!updateMovie) {
+            return res.status(400).json({ error: "movie not found" });
+        }
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
+
+
 //delete operation using movie id
-app.delete('/movie/:id', async(req, res)=>{
-    try{
-    const deleteMovie = await movie.findByIdAndDelete(req.params.id);
-    res.status(200).json(deleteMovie);
+app.delete('/movie/:id', async (req, res) => {
+    try {
+        const deleteMovie = await movie.findByIdAndDelete(req.params.id);
+        res.status(200).json(deleteMovie);
     }
-    catch(error){
-        res.status(500).json({message: error.message});
+    catch (error) {
+        res.status(500).json({ message: error.message });
     }
 })
 
--- test/movie.test.mjs
+++ test/movie.test.mjs
@@ -3,18 +3,18 @@ import request from 'supertest';
 import mongoose from 'mongoose';
 import movie from '../model/movie.mjs';
 
-beforeAll(async ()=>{
+beforeAll(async () => {
     mongoose.connect('mongodb+srv://daisysarma20:WZqayT7Twe9hjPDP@cluster0.juzlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
         useNewUrlParser: true,
         useUnifiedTopology: true,
     });
 });
 
-beforeEach(async()=>{
+beforeEach(async () => {
     await movie.deleteMany({});
 })
 
-afterAll(async()=>{
+afterAll(async () => {
     await mongoose.connection.close();
 })
 
@@ -26,20 +26,131 @@ describe('Movie api', () => {
             genre: "Romance/Drama",
             releaseYr: "2018"
         });
-  
+
         expect(response.statusCode).toBe(201);
         expect(response.body.title).toBe("October")
     }, 50000);
 
-    it('should return a error if the movie already exist', async() =>{
-        const response = await request(app).post('/movie').send({
-            title: "October",
-            director: "Shoojit Sircar",
-            genre: "Romance/Drama",
-            releaseYr: "2018"
+    it('should return a error if the movie already exist', async () => {
+        await request(app).post('/movie').send({
+            title: "Shutter Island",
+            director: "Martin Scorsese",
+            genre: "Thriller/Mystery ",
+            releaseYr: "2010"
         });
-       
-        expect(response.statusCode).toBe(409);
-        expect(response.text).toBe("this movie already exist")
+
+        const response = await request(app).post('/movie').send({
+            title: "Shutter Island",
+            director: "Martin Scorsese",
+            genre: "Thriller/Mystery ",
+            releaseYr: "2010"
+        });
+        expect(response.status).toBe(409);
+        expect(response.body).toBe("Movie with this name already exist !");
     }, 30000);
-});
+
+    it('should return all the movies', async () => {
+        await request(app).post('/movie').send({
+            title: "Asian Island",
+            director: "Martin Scorsese",
+            genre: "Thriller/Mystery ",
+            releaseYr: "2010"
+        });
+        const response = await request(app).get('/movie');
+        expect(response.statusCode).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBe(1);
+    });
+
+    test('should return a movie by ID', async () => {
+        const createResponse = await request(app).post('/movie').send({
+            title: "Aamir",
+            director: "Raj Kumar Gupta",
+            genre: "Thriller/Action",
+            releaseYr: "2008"
+        });
+
+        expect(createResponse.status).toBe(201);
+        const movieId = createResponse.body._id;
+
+        const response = await request(app).get(`/movie/${movieId}`);
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('_id', movieId);
+        expect(response.body.title).toBe("Aamir");
+
+
+    }, 5000);
+
+    it('should return 400 error for invalid movie id', async()=>{
+        await request(app).post('/movie').send({
+            title: "Asian Island",
+            director: "Martin Scorsese",
+            genre: "Thriller/Mystery ",
+            releaseYr: "2010"
+        });
+
+        const invalidID = "12bjkhaos1";
+
+        const response = await request(app).get(`/movie/${invalidID}`);
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error',"Invalid movie ID")
+    });
+
+    it('should update a movie by movie id', async () => {
+        const createResponse = await request(app).post('/movie').send({
+            title: "Love Money",
+            director: "Vishal Mahadkar",
+            genre: "Action/Thriller",
+            releaseYr: "2012"
+        });
+
+        expect(createResponse.status).toBe(201);
+        const movieId = createResponse.body._id;
+
+        const response = await request(app).put(`/movie/${movieId}`).send({
+            title: "Blood Money"
+        });
+
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toHaveProperty('_id', movieId);
+        expect(response.body.title).toBe("Blood Money");
+    });
+
+    it('should return an 400 error while trying to update movie data using invalid movie id', async()=>{
+        await request(app).post('/movie').send({
+            title: "Love Money",
+            director: "Vishal Mahadkar",
+            genre: "Action/Thriller",
+            releaseYr: "2012"
+        });
+
+        const invalidId = "13bhou3kh3oiue908efih";
+        const response = await request(app).get(`/movie/${invalidId}`).send({
+            title: "Blood Money"
+        });
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error',"Invalid movie ID");
+        
+    });
+
+    it('should return an 400 error while trying to update movie data with missing fields', async()=>{
+        const createResponse = await request(app).post('/movie').send({
+            title: "Love Money",
+            director: "Vishal Mahadkar",
+            genre: "Action/Thriller",
+            releaseYr: "2012"
+        });
+
+        expect(createResponse.status).toBe(201);
+        const movieId = createResponse.body._id;
+
+        const response = await request(app).put(`/movie/${movieId}`).send();
+
+        expect(response.statusCode).toBe(400);
+        expect(response.body).toHaveProperty('error', "At least one field is required to update.");
+        
+    });
+
+}); 
