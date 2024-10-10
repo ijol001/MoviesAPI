31da34c71cf8d8b0673700f7afe32c3d03ef8020
ijol001 <daisysarma20@gmail.com>
2024-10-10 11:27:11 +0530

    commit

--- test/movie.test.mjs
+++ test/movie.test.mjs
@@ -1,6 +1,22 @@
 import app from '../index.mjs';
 import request from 'supertest';
+import mongoose from 'mongoose';
+import movie from '../model/movie.mjs';
 
+beforeAll(async ()=>{
+    mongoose.connect('mongodb+srv://daisysarma20:WZqayT7Twe9hjPDP@cluster0.juzlr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+});
+
+beforeEach(async()=>{
+    await movie.deleteMany({});
+})
+
+afterAll(async()=>{
+    await mongoose.connection.close();
+})
 
 describe('Movie api', () => {
     it('should create a movie', async () => {
@@ -11,7 +27,19 @@ describe('Movie api', () => {
             releaseYr: "2018"
         });
   
-        expect(res.statusCode).toEqual(201)
-        expect(res.body).toHaveProperty('post')
+        expect(response.statusCode).toBe(201);
+        expect(response.body.title).toBe("October")
     }, 50000);
-})
+
+    it('should return a error if the movie already exist', async() =>{
+        const response = await request(app).post('/movie').send({
+            title: "October",
+            director: "Shoojit Sircar",
+            genre: "Romance/Drama",
+            releaseYr: "2018"
+        });
+       
+        expect(response.statusCode).toBe(409);
+        expect(response.text).toBe("this movie already exist")
+    }, 30000);
+});
